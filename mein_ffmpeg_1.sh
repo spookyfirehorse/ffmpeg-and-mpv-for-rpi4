28833a6a74d1f0f8108870effd12763f3ce1f684
spookyfirehorse <47276604+spookyfirehorse@users.noreply.github.com>
2019-02-02 16:48:36 +0100

    Update mein_ffmpeg_1.sh

--- mein_ffmpeg_1.sh
+++ mein_ffmpeg_1.sh
@@ -9,11 +9,14 @@ wget http://us.download.nvidia.com/XFree86/Linux-x86_64/410.93/NVIDIA-Linux-x86_
 
 nano /etc/ld.so.conf
 
+
+/usr/local/lib
 /usr/local/cuda-10.0/lib64/
 
 ldconfig
 
 mkdir ~/ffmpeg_sources
+mkdir ~/bin
 
 cd ~/ffmpeg_sources && \
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git  && \
@@ -141,7 +144,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-nonfree \
   --enable-libsoxr \
   --enable-libvpx \
-  --enable-x86asm \
   --enable-libpulse \
   --enable-libssh --enable-libxvid \
   --enable-libass  --enable-avisynth  --enable-libspeex \
@@ -154,4 +156,9 @@ PATH="$HOME/bin:$PATH" make -j10  && \
 make install && \
 hash -r
 
+ssh username@host 
 
+# audiodevice
+arecord -L
+
+ssh username@host ffmpeg  -f alsa -ac 2 -i plughw:CARD=VF0770,DEV=0 -f v4l2  -i /dev/video0 -c:v h264_nvenc -preset fast -b:v 300k  -tune zerolatency -r 10 -c:a libfdk_aac -b:a 128k  -ar 48000  -af aresample=async=1:min_hard_comp=0.100000:first_pts=0  -y -threads 4  -f matroska - | ffplay -vcodec h264_cuvid -fflags nobuffer -
