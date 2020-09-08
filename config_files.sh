b9fcca578a5e728cd5ffcc904b39bded555fd776
spookyfirehorse <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-09 01:07:00 +0200

    Add files via upload

---
+++ config_files.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/spookyfirehorse/raspberry-pi-with-ffmpeg-all-codecs-mpv-rpi-.git && \
+cd ~/raspberry-pi-with-ffmpeg-all-codecs-mpv-rpi- &&  \
+cp bashrc ~/.bashrc &&  mkdir -p ~/.config/mpv && cp mpv.conf ~/.config/mpv/ && mkdir -p ~/.config/vlc && cp vlcrc ~/.config/vlc/
