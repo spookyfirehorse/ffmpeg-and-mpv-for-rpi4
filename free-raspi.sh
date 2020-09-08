693a74edcb311196336960b1a9a7ae11927cee70
spookyfirehorse <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-09 00:45:27 +0200

    Update free-raspi.sh

--- free-raspi.sh
+++ free-raspi.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/spookyfirehorse/raspberry-pi-with-ffmpeg-all-codecs-mpv-rpi-.git && \
 cd ~/raspberry-pi-with-ffmpeg-all-codecs-mpv-rpi- &&  \
 cp bashrc ~/.bashrc &&  mkdir -p ~/.config/mpv && cp mpv.conf ~/.config/mpv/ && mkdir -p ~/.config/vlc && cp vlcrc ~/.config/vlc/ && \
-sudo apt -y  install autoconf automake build-essential pkg-config devscripts cmake doxygen git graphviz imagemagick libasound2-dev libass-dev libfreetype6-dev \
+apt install autoconf automake build-essential pkg-config devscripts cmake doxygen git graphviz imagemagick libasound2-dev libass-dev libfreetype6-dev \
 libgmp-dev  libopus-dev librtmp-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev \
 libsnappy-dev libsoxr-dev libssl-dev  libv4l-dev libva-dev libvorbis-dev libx264-dev libxcb-shape0-dev \
 libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev libxml2-dev lzma-dev nasm python3-dev python3-pip texinfo wget yasm zlib1g-dev youtube-dl \
@@ -10,10 +10,10 @@ libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidc
 libfreetype6-dev libsdl2-dev libtool libvdpau-dev  libxcb1-dev libvpx-dev libpulse-dev  libwebp-dev libxvidcore-dev \
 libzvbi-dev libxcb-shm0-dev  libv4l-dev libxcb-xfixes0-dev libvpx-dev texinfo libpulse-dev libomxil-bellagio-dev libssh-gcrypt-dev \
 libssl-dev wget zlib1g-dev nasm yasm libx264-dev   libnuma-dev libgles2-mesa-dev equivs meson spirv-tools libplacebo* \
-libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev libx11-xcb-dev libqt5*-dev qt5*-dev \
-   libgstreamer-plugins-base1.0-dev ocl-icd-opencl-dev ocl-icd-dev qtquickcontrols2-5-dev qtmultimedia5-dev&& sudo apt -y purge mpv 
- 
- 
+libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev libx11-xcb-dev libqt5*-dev qt5*-dev && \ 
+apt install --no-install-recommends vlc-data qml-module-qtquick-layouts qml-module-org-kde-newstuff  vlc-plugin-base vlc-plugin-video-output libvlc5 libvlccore9 qml-module-qtquick-layouts
+
+ #############################################
 libfdkaac-dev
 
 mkdir -p ~/ffmpeg_sources && \
