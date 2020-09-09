59e739d447bd5da70940b960d1632fcf5b7b3781
spookyfirehorse <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-09 22:41:53 +0200

    Update mpv.sh

--- mpv.sh
+++ mpv.sh
@@ -1,8 +1,11 @@
+export CPATH=~/ffmpeg_build/include && \
+export LD_LIBRARY_PATH=~/ffmpeg_build/lib && \
+export PKG_CONFIG_PATH=~/ffmpeg_build/lib/pkgconfig:/opt/vc/lib/pkgconfig  && \
 cd ~/ffmpeg_sources  && \
 git clone https://github.com/mpv-player/mpv.git && \
 cd mpv && \
 ./bootstrap.py && \
 ./waf configure --enable-rpi --enable-rpi-mmal --disable-vaapi --enable-egl-drm --enable-egl-x11  --disable-wayland --disable-android --disable-vdpau --disable-vulkan && \
 ./waf -j4 && sudo  ./waf install && \
-sudo apt build-dep vlc && \
+sudo apt build-dep vlc
 
