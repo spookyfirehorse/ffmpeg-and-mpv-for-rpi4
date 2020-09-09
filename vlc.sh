304134f6798cd791255ec49458ba15631ddc5d61
spookyfirehorse <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-09 23:00:37 +0200

    Update vlc.sh

--- vlc.sh
+++ vlc.sh
@@ -4,6 +4,6 @@ cd vlc && ./bootstrap && \
 --enable-mmal --enable-omxil --enable-rpi-omxil  \
 --disable-libva --enable-fdkaac --enable-gles2  --disable-vdpau --disable-wayland  && \
 make -j4 && sudo make install && sudo  ldconfig && \
-sudo apt purge libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev libvlc* vlc*  ffmpeg mpv && \
+sudo apt purge libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev libvlc* vlc* vlc-bin ffmpeg mpv && \
 sudo apt-mark manual opencv* && sudo apt-mark manual lib* && sudo apt-mark manual *-dev && \
 sudo apt-mark hold libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev 
