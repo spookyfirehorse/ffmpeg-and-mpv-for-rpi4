bf3b6b0d05b89a23aa6f9dfaf3a49c843e644c12
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-10 23:47:25 +0200

    Update vlc.sh

--- vlc.sh
+++ vlc.sh
@@ -10,4 +10,4 @@ sudo apt purge -y  vlc vlc-bin vlc-plugin* libvlc* ffmpeg mpv  && \
 cd ~/ && sudo apt-mark manual opencv* && sudo apt-mark manual lib* && sudo apt-mark manual *-dev  &&  sudo apt-mark manual qml-module* && \
 sudo apt-mark hold libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev  && \
 sudo apt install -y --no-install-recommends  qml-module-qtquick-layouts qml-module-org-kde-newstuff \
-vlc-plugin-video-output libvlc5 libvlccore9  libvlc-bin  vlc-data phonon4qt5-backend-vlc && sudo ldconfig 
+vlc-plugin-video-output libvlc5 libvlccore9  libvlc-bin  vlc-data phonon4qt5-backend-vlc phonon-backend-vlc  && sudo ldconfig 
