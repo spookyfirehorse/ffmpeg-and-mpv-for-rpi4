405623519a182c25ffe508c9578838b0484dfe1a
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-11 00:46:33 +0200

    Update config_files.sh

--- config_files.sh
+++ config_files.sh
@@ -1,1 +1,1 @@
-cp bashrc ~/.bashrc &&  mkdir -p ~/.config/mpv && cp mpv.conf ~/.config/mpv/ && mkdir -p ~/.config/vlc && cp vlcrc ~/.config/vlc/ && sudo cp bashrc /root/.bashrc && sudo cp sources.list /etc/apt/
+cp pcmanfm.conf ~/.config/pcmanfm/LXDE-pi/  && mkdir -p ~/ffmpeg_sources && mkdir -p ~/bin && cp bashrc ~/.bashrc &&  mkdir -p ~/.config/mpv && cp mpv.conf ~/.config/mpv/ && mkdir -p ~/.config/vlc && cp vlcrc ~/.config/vlc/ && sudo cp bashrc /root/.bashrc && sudo cp sources.list /etc/apt/
