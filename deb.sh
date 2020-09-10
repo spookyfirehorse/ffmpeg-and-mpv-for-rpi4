e0b2ee05ecb2ff9b2a62cc56c41a54c9bebd07e3
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-10 05:57:13 +0200

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,5 +1,5 @@
 apt build-dep ffmpeg && \
-sudo apt install -y xcb-xkb*-dev libmp3lame-dev  autoconf automake build-essential pkg-config devscripts cmake  imagemagick libasound2-dev libass-dev libfreetype6-dev \
+sudo apt install -y xcb-xkb*-dev libvpx-dev libssh-gcrypt-dev libmp3lame-dev  autoconf automake build-essential pkg-config devscripts cmake  imagemagick libasound2-dev libass-dev libfreetype6-dev \
 libgmp-dev  libopus-dev librtmp-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev \
 libsnappy-dev libsoxr-dev libssl-dev  libv4l-dev libva-dev  doxygen git graphvizlibvorbis-dev libx264-dev libxcb-shape0-dev \
 libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev libxml2-dev lzma-dev nasm python3-dev python3-pip texinfo wget yasm zlib1g-dev youtube-dl \
@@ -11,4 +11,4 @@ libssl-dev wget zlib1g-dev nasm yasm libx264-dev   libnuma-dev libgles2-mesa-dev
 libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev libx11-xcb-dev libqt5*-dev qt5*-dev && \ 
 sudo apt install --no-install-recommends  qml-module-qtquick-layouts qml-module-org-kde-newstuff  vlc-plugin-base  \
 vlc-plugin-video-output libvlc5 libvlccore9 qml-module-qtquick-layouts && \
-sudo apt build-dep mpv && sudo apt build-dep vlc
+sudo apt build-dep mpv && sudo apt build-dep vlc 
