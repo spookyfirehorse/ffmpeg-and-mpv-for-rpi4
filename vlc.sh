02601cd1d63f48aee2c2d71d456eba824eb87b80
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-10 17:32:35 +0200

    Update vlc.sh

--- vlc.sh
+++ vlc.sh
@@ -6,8 +6,8 @@ cd vlc && ./bootstrap && \
 --disable-libva --enable-fdkaac --enable-gles2  --disable-vdpau --disable-wayland  && \
 make -j4 && sudo make install && sudo  ldconfig && \
 sudo apt purge -y libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev  && \
-sudo apt purge -y  vlc vlc-bin ffmpeg mpv  && \
+sudo apt purge -y  vlc vlc-bin vlc-plugin* libvlc* ffmpeg mpv  && \
 cd ~/ && sudo apt-mark manual opencv* && sudo apt-mark manual lib* && sudo apt-mark manual *-dev  &&  sudo apt-mark manual qml-module* && \
 sudo apt-mark hold libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev  && \
-sudo apt install -y --no-install-recommends  qml-module-qtquick-layouts qml-module-org-kde-newstuff libvlc-bin  vlc-plugin-base vlc-plugin-qt vlc-plugin-skins2 \
-vlc-plugin-video-output libvlc5 libvlccore9 vlc-plugin-base vlc-data && sudo ldconfig 
+sudo apt install -y --no-install-recommends  qml-module-qtquick-layouts qml-module-org-kde-newstuff \
+vlc-plugin-video-output libvlc5 libvlccore9  libvlc-bin  vlc-data phonon4qt5-backend-vlc && sudo ldconfig 
