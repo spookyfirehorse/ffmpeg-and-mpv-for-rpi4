2a28e481173d2fe542d3498bcf1d82609b29c827
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-10 09:38:32 +0200

    Update install-equalizer.sh

--- install-equalizer.sh
+++ install-equalizer.sh
@@ -1,6 +1,6 @@
 sudo apt install python3-pip bc swh-plugins meson && sudo  pip3 install ninja && \
 cd ~/ffmpeg_sources && git clone https://github.com/pulseaudio-equalizer-ladspa/equalizer.git &&\
-cd equalizer
+cd equalizer && \
 meson build && \
 cd build && \
 ninja && \
