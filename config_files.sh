3bc7570c2d883c26cbc2bbf5d6db73cb53b9b7f4
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-10 10:36:30 +0200

    Update config_files.sh

--- config_files.sh
+++ config_files.sh
@@ -1,1 +1,1 @@
-cp bashrc ~/.bashrc &&  mkdir -p ~/.config/mpv && cp mpv.conf ~/.config/mpv/ && mkdir -p ~/.config/vlc && cp vlcrc ~/.config/vlc/ && sudo cp bashrc /root/.bashrc
+cp bashrc ~/.bashrc &&  mkdir -p ~/.config/mpv && cp mpv.conf ~/.config/mpv/ && mkdir -p ~/.config/vlc && cp vlcrc ~/.config/vlc/ && sudo cp bashrc /root/.bashrc && sudo cp sources.list /etc/apt/
