d71c415a7d3e4c42ac7545ec38d8036f430b80fa
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-10 09:24:28 +0200

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -6,9 +6,11 @@ libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev libxml2-dev lzma-dev nasm python3
 libgtk2.0-dev  python3-numpy python-dev libtiff5-dev libjasper-dev libdc1394-22-dev libv4l-dev libtbb-dev libmp3lame-dev \
 libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils mousepad git-core libass-dev \
 libfreetype6-dev libsdl2-dev libtool libvdpau-dev  libxcb1-dev libvpx-dev libpulse-dev  libwebp-dev libxvidcore-dev \
-libzvbi-dev libxcb-shm0-dev  libv4l-dev libxcb-xfixes0-dev libvpx-dev texinfo libpulse-dev libomxil-bellagio-dev libssh-gcrypt-dev \
+libzvbi-dev libxcb-shm0-dev  libv4l-dev libxcb-xfixes0-dev libvpx-dev texinfo libpulse-dev pavucontrol libomxil-bellagio-dev libssh-gcrypt-dev \
 libssl-dev wget zlib1g-dev nasm yasm libx264-dev   libnuma-dev libgles2-mesa-dev equivs meson spirv-tools libplacebo* flatpak \
-libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev libx11-xcb-dev libqt5*-dev qt5*-dev libqt5quick* qtquickcontrols2-5-dev && \ 
+libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev \
+libx11-xcb-dev libqt5*-dev qt5*-dev libqt5quick* qtquickcontrols2-5-dev && \
+sudo apt install -y python3-pip bc swh-plugins meson && sudo pip3 install ninja && \
 sudo apt install --no-install-recommends  qml-module-qtquick-layouts qml-module-org-kde-newstuff  vlc-plugin-base  \
 vlc-plugin-video-output libvlc5 libvlccore9 qml-module-qtquick-layouts   && \
 sudo apt build-dep mpv && sudo apt build-dep vlc 
