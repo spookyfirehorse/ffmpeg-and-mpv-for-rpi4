cd2f1816c1d9c88ffa09a97ad0fef8c0b2f25922
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-11 15:28:38 +0200

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,16 +1,17 @@
-sudo apt purge -y libavcodec-dev libavdevice-dev libswresample-dev libpostproc-dev libswscale-dev libavformat-dev  ffmpeg mpv   && \
 mkdir -p ~/bin &&  \
 cd ~/ffmpeg_sources && wget https://ffmpeg.org/releases/ffmpeg-4.3.1.tar.bz2 && tar xjvf ffmpeg-4.3.1.tar.bz2 && cd ffmpeg-4.3.1 && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
 ./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" \
 --extra-ldflags="-L$HOME/ffmpeg_build/lib" --extra-libs="-lpthread -lm" --bindir="$HOME/bin" \
---enable-gpl --enable-libaom --enable-libass --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libv4l2 \
---enable-libxvid --enable-sdl2 --enable-libx264 --enable-pthreads --enable-openssl --enable-nonfree --disable-debug --enable-libsoxr --enable-version3 \
---enable-libvpx --disable-doc --disable-htmlpages --enable-libpulse --enable-libssh --disable-manpages --disable-podpages --enable-opengl --enable-libzvbi \
---enable-avfilter --enable-filters --enable-mmal --enable-omx --enable-omx-rpi --enable-decoder=h264_mmal --enable-decoder=mpeg2_mmal --enable-encoder=h264_omx  \
---enable-runtime-cpudetect --enable-libwebp --disable-avresample --enable-swresample --enable-x86asm --enable-libjack --enable-shared --disable-static \
---enable-librubberband --disable-vdpau --disable-vaapi --enable-gmp --enable-hardcoded-tables --disable-stripping && \
-PATH="$HOME/bin:$PATH" make -j 4 && sudo make install && cd ~/bin && sudo cp ff* /usr/local/bin/ && \
-export CPATH=~/ffmpeg_build/include && \
-export LD_LIBRARY_PATH=~/ffmpeg_build/lib && \
-export PKG_CONFIG_PATH=~/ffmpeg_build/lib/pkgconfig:/opt/vc/lib/pkgconfig 
+--enable-gpl --disable-stripping --enable-libsoxr --enable-libfdk-aac --enable-nonfree --enable-libpulse  --enable-avresample --disable-filter=resample  \
+--enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libflite \
+ --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg \
+ --enable-libopenmpt --enable-libopus --enable-libpulse --enable-librsvg --enable-librubberband --enable-libshine --enable-libsnappy  --enable-libspeex --enable-libssh \
+ --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid \
+ --enable-libzmq --disable-doc --disable-htmlpages --disable-manpages  --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opengl --enable-sdl2\
+ --enable-mmal --enable-omx --enable-omx-rpi --enable-decoder=h264_mmal --enable-decoder=mpeg2_mmal --enable-encoder=h264_omx  --enable-neon  --enable-libdc1394 --enable-libdrm --enable-libiec61883 \
+ --enable-chromaprint --enable-frei0r --enable-libx264 --enable-static --libdir=/usr/lib/arm-linux-gnueabihf --cpu=arm1176jzf-s --arch=arm   && \
+PATH="$HOME/bin:$PATH" make -j 4 && make install
+#export CPATH=~/ffmpeg_build/include && \
+#export LD_LIBRARY_PATH=~/ffmpeg_build/lib && \
+##export PKG_CONFIG_PATH=~/ffmpeg_build/lib/pkgconfig:/opt/vc/lib/pkgconfig 
