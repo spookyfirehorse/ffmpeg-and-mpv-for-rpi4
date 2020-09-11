870c3e8d272d1860a7c53af2a8692fb41ca69195
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-11 21:20:12 +0200

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,4 +1,5 @@
 sudo apt update && sudo apt upgrade -y && sudo apt build-dep -y  ffmpeg && \
-sudo apt install -y python3-pip bc swh-plugins meson libunistring-dev meson spirv-tools libplacebo* flatpak libgles2-mesa-dev && \
+sudo apt install -y python3-pip bc swh-plugins meson libunistring-dev meson spirv-tools libplacebo* flatpak libgles2-mesa-dev \
+libv4l-dev libx265-dev  libopencore-amrnb-dev libopencore-amrwb-dev libx264-dev && \
 sudo apt build-dep -y mpv 
 
