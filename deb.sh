ae53548dc336c622d6da4ea0f264cc400d95fe5d
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-11 19:51:34 +0200

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,16 +1,6 @@
 sudo apt update && sudo apt upgrade -y && sudo apt build-dep -y  ffmpeg && \
-sudo apt install -y xcb-xkb*-dev libvlccore-dev libvlc-dev libdrm-dev  libvpx-dev libssh-gcrypt-dev libmp3lame-dev  autoconf automake build-essential pkg-config devscripts cmake  imagemagick libasound2-dev libass-dev libfreetype6-dev \
-libgmp-dev  libopus-dev librtmp-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev \
-libsnappy-dev libsoxr-dev libssl-dev  libv4l-dev libva-dev  doxygen git graphviz libvorbis-dev  libxcb-shape0-dev \
-libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev libxml2-dev lzma-dev nasm python3-dev python3-pip texinfo wget yasm zlib1g-dev youtube-dl \
-libgtk2.0-dev  python3-numpy python-dev libtiff5-dev libjasper-dev libdc1394-22-dev libv4l-dev libtbb-dev libmp3lame-dev \
-libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils mousepad git-core libass-dev \
-libfreetype6-dev libsdl2-dev libtool libvdpau-dev  libxcb1-dev libvpx-dev libpulse-dev  libwebp-dev libxvidcore-dev \
-libzvbi-dev libxcb-shm0-dev  libv4l-dev libxcb-xfixes0-dev libvpx-dev texinfo libpulse-dev pavucontrol libomxil-bellagio-dev libssh-gcrypt-dev \
-libssl-dev wget zlib1g-dev nasm yasm libx264-dev   libnuma-dev libgles2-mesa-dev equivs meson spirv-tools libplacebo* flatpak \
-libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev \
+sudo apt install -y python3-pip bc swh-plugins meson && sudo pip3 install ninja libunistring-dev meson spirv-tools libplacebo* flatpak \
+libxcb-shm0-dev libxcb-xv0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-composite0-dev libgles2-mesa-dev xcb-xkb*-dev libvlccore-dev libvlc-dev libdrm-dev\
 libx11-xcb-dev libqt5*-dev qt5*-dev libqt5quick* qtquickcontrols2-5-dev && \
- sudo apt install -y python3-pip bc swh-plugins meson && sudo pip3 install ninja libunistring-dev && \
 sudo apt build-dep -y mpv && sudo apt build-dep -y vlc && \
-sudo apt install -y --no-install-recommends  qml-module-qtquick-layouts qml-module-org-kde-newstuff \
-vlc-plugin-video-output libvlc5 libvlccore9  libvlc-bin  vlc-data phonon4qt5-backend-vlc
+
