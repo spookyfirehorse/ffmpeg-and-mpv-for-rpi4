dac45243f811368a29c155f0f105645b514453ed
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-12 08:02:49 +0200

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,5 +1,5 @@
-sudo apt update && sudo apt upgrade -y && sudo apt build-dep -y  ffmpeg && \
+sudo apt update && sudo apt upgrade -y && sudo apt build-dep -y  ffmpeg && sudo apt build-dep -y mpv \
 sudo apt install -y python3-pip bc swh-plugins meson libunistring-dev meson spirv-tools libplacebo* flatpak libgles2-mesa-dev \
-libv4l-dev libx265-dev  libopencore-amrnb-dev libopencore-amrwb-dev libx264-dev && \
-sudo apt build-dep -y mpv 
+libv4l-dev libx265-dev glslang-dev libopencore-amrnb-dev libopencore-amrwb-dev libx264-dev  \
+libvulkan* python3-mako  libshaderc* liblcms2* libepoxy*  && sudo pip3 install ninja
 
