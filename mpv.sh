f2f962530d83df51912ec723903e826d9f2cce3f
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-13 04:33:03 +0200

    Update mpv.sh

--- mpv.sh
+++ mpv.sh
@@ -4,7 +4,7 @@ cd ~/ffmpeg_sources  && \
 git clone https://github.com/mpv-player/mpv.git && \
 cd mpv && \
 ./bootstrap.py && \
-./waf configure --enable-rpi --enable-rpi-mmal --disable-vaapi --enable-egl-drm --enable-egl-x11  --disable-wayland --disable-android --disable-vdpau --disable-vulkan && \
+./waf configure --enable-rpi --enable-rpi-mmal --disable-vaapi --enable-egl-drm --enable-egl-x11  --disable-wayland --disable-android --disable-vdpau && \
 ./waf -j4 && sudo  ./waf install 
 
 
