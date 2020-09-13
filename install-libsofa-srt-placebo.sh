c10635c7f35fc75fa2df0c0b9713e15250062df7
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-13 04:15:14 +0200

    Update install-libsofa-srt-placebo.sh

--- install-libsofa-srt-placebo.sh
+++ install-libsofa-srt-placebo.sh
@@ -1,20 +1,20 @@
 #sudo pip3 install ninja && \
-cd ~/ffmpeg_sources && git clone https://github.com/hoene/libmysofa.git && \
-apt install zlib1g-dev libcunit1-dev libcunit1-dev && \
-cd build  && \
-cmake -DCMAKE_BUILD_TYPE=Debug ..  && \
-cd build && cpack  && \
-sudo apt purge libmysofa-dev  && \
-sudo dpkg -i libmysofa-1.0.0-Linux.deb && \
+sudo apt install zlib1g-dev libcunit1-dev libcunit1-dev  && \
+cd ~/ffmpeg_sources && git clone https://github.com/hoene/libmysofa.git && cd libmysofa/build  && \
+cmake -DCMAKE_BUILD_TYPE=Debug .. && cd ~/ffmpeg_sources/libmysofa/build && cpack  && \
+sudo apt purge -y libmysofa-dev  && \
+sudo dpkg -i libmysofa-1.0.0-Linux.deb && cd ~/ffmpeg_sources && \
 git clone https://github.com/AviSynth/AviSynthPlus.git && \
 cd AviSynthPlus && /
 mkdir avisynth-build && cd avisynth-build && \ 
 cmake ../ -DHEADERS_ONLY:bool=on  && \
-sudo make install && \  
-sudo apt install glslang* spirv-toolsapt install glslang* spirv-tools && \
+sudo make install 
+  
+sudo apt install glslang* spirv-tools && sudo apt install glslang* spirv-tools && \
+cd ~/ffmpeg_sources
 sudo apt purge libplacebo* && \
 git clone https://github.com/haasn/libplacebo.git && \
-cd libplacebo && /
+cd ~/ffmpeg_sources/libplacebo && /
 DIR=./build && \
 meson $DIR  && \
 ninja -C$DIR  && \
