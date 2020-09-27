85b568e7ef29abd305a04f91c8108eb9b42e9b06
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2020-09-27 14:27:19 +0200

    Update ffmpeg2.sh

--- ffmpeg2.sh
+++ ffmpeg2.sh
@@ -1,19 +1,16 @@
 mkdir -p ~/bin &&  \
 mkdir -p ~/ffmpeg_sources &&  \
-cd ~/ffmpeg_sources && git clone https://github.com/FFmpeg/FFmpeg.git && cd FFmpeg && \
-PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
-./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" \
---extra-ldflags="-L$HOME/ffmpeg_build/lib" --extra-libs="-lpthread -lm" --bindir="$HOME/bin" \
---enable-gpl --disable-stripping --enable-libsoxr --enable-libfdk-aac --enable-nonfree --enable-libpulse  --enable-avresample --disable-filter=resample  \
---enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libflite \
- --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg \
- --enable-libopenmpt --enable-libopus --enable-libssh --enable-librsvg --enable-librubberband --enable-libshine --enable-libsnappy  --enable-libspeex --enable-libssh \
- --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid \
- --enable-libzmq --disable-doc --disable-htmlpages --disable-manpages  --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opengl --enable-sdl2\
- --enable-mmal --enable-omx --enable-omx-rpi --enable-decoder=h264_mmal --enable-decoder=mpeg2_mmal --enable-encoder=h264_omx  --enable-neon  --enable-libdc1394 --enable-libdrm --enable-libiec61883 \
- --enable-chromaprint --enable-frei0r --enable-libx264 --enable-avisynth --enable-libsrt --enable-shared  && \
-PATH="$HOME/bin:$PATH" make -j 4 && make install
-
-#export CPATH=~/ffmpeg_build/include && \
-#export LD_LIBRARY_PATH=~/ffmpeg_build/lib && \
-##export PKG_CONFIG_PATH=~/ffmpeg_build/lib/pkgconfig:/opt/vc/lib/pkgconfig 
+cd ~/ffmpeg_sources && git clone https://github.com/FFmpeg/FFmpeg.git && cd FFmpeg 
+./configure --prefix=/usr/local --enable-gpl --enable-cuda --enable-cuda-nvcc --enable-cuda-llvm  --enable-vaapi \
+--enable-nvdec --enable-cuvid --enable-nvenc --enable-ffnvcodec --enable-libnpp  --enable-nvenc --enable-ffnvcodec \
+--enable-libnpp --enable-vapoursynth --disable-stripping --enable-libsoxr --enable-libpulse --enable-avresample --disable-filter=resample \
+--enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca \
+--enable-libcdio --enable-libcodec2 --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi \
+--enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt \
+--enable-libopus --enable-libssh --enable-librsvg --enable-librubberband --enable-libshine --enable-libsnappy --enable-libspeex \
+--enable-libssh --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwavpack \
+--enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzmq --disable-doc --disable-htmlpages \
+--disable-manpages --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opengl --enable-sdl2 --enable-omx \
+--enable-libdc1394 --enable-libdrm  --enable-chromaprint --enable-frei0r --enable-libx264 --enable-avisynth --enable-libsrt \
+--enable-shared --enable-libfdk-aac --enable-nonfree --enable-liblensfun --enable-libopencore_amrnb --enable-version3 && \
+make -j4 && sudo make install && make tools/qt-faststart && sudo cp  tools/qt-faststart /usr/local/bin/ && ldconfig
