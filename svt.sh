1b96abbbbff34d61384aa2b750dacc23bd97bae0
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-06-12 09:51:21 +0200

    Create svt.sh

---
+++ svt.sh
@@ -0,0 +1,14 @@
+cd ~/ffmpeg_sources && \
+git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git && \
+mkdir -p SVT-AV1/build && \
+cd SVT-AV1/build && \
+ cmake -G "Unix Makefiles"  -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
+make -j4 && \
+sudo make install && \
+cd ~/ffmpeg_sources && \
+git -C dav1d pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/dav1d.git && \
+mkdir -p dav1d/build && \
+cd dav1d/build && \
+meson setup -Denable_tools=false -Denable_tests=false --default-library=static ..  && \
+ninja && \
+sudo ninja install
