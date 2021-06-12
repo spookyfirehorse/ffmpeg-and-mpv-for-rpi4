b23e327e55a9671cf99ef2e3711c12a6d5fa93d1
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-06-12 09:41:50 +0200

    Update and rename ffmpeg.sh to ffmpeg-rpi4 + rpi3b+.sh

--- ffmpeg.sh
+++
@@ -1,9 +0,0 @@
-
-mkdir -p ~/bin &&  \
-mkdir -p ~/ffmpeg_sources &&  \
-cd ~/ffmpeg_sources && git clone https://github.com/FFmpeg/FFmpeg.git  &&  \ 
-cd ~/ffmpeg_sources/FFmpeg &&  \
-./configure --prefix=/usr --extra-version=4 --toolchain=hardened --libdir=/usr/lib/arm-linux-gnueabihf --incdir=/usr/include/arm-linux-gnueabihf --enable-gpl --disable-stripping --enable-libsoxr --enable-libpulse --enable-swresample --enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt --enable-libopus --enable-libssh --enable-librsvg --enable-librubberband --enable-libshine --enable-libsnappy --enable-libspeex --enable-libssh --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzmq --disable-doc --disable-htmlpages --disable-manpages --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opengl --enable-sdl2 --enable-mmal --enable-omx-rpi --enable-decoder=h264_mmal --enable-decoder=mpeg2_mmal --enable-encoder=h264_omx --enable-libdc1394 --enable-libdrm --enable-libiec61883 --enable-chromaprint --enable-frei0r --enable-libx264 --enable-avisynth --enable-libsrt --enable-shared --enable-libfdk-aac --enable-nonfree --enable-liblensfun --enable-libopencore_amrnb --enable-version3
-make -j4 &&  \
-sudo make install &&  \
-make tools/qt-faststart && sudo cp  tools/qt-faststart /usr/local/bin/ && ldconfig
