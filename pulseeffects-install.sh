2adceb3f9864fe46bcde64c384f2073fec215978
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-06-15 16:42:25 +0200

    Update pulseeffects-install.sh
    version 4.8.5 only bullseye or ubuntu 22.04
    debian stable 4.7.3 
    
    
    git checkout v4.7.3

--- pulseeffects-install.sh
+++ pulseeffects-install.sh
@@ -4,7 +4,7 @@ cd ~/ffmpeg_sources/lsp-plugins && ./build-armv7a.sh && \
 make -j4 && \
 sudo make install %% \
 sudo apt install -y libfftw3-bin libfftw3-dev  && \
-cd ~/ffmpeg_sources && git clone https://github.com/swh/ladspa.git  && \
+cd ~/ffmpeg_sources && git clone https://github.com/swh/ladspa.git &&  cd ~/ffmpeg_sources/ladspa && \
 autoreconf -i && \
 ./configure && \
 make -j4 && \
