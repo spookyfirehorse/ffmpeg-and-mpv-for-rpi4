082e71e48f58f0a4311c496335fadf1a51eabe42
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-06-15 16:44:28 +0200

    Update pulseeffects-install.sh

--- pulseeffects-install.sh
+++ pulseeffects-install.sh
@@ -10,9 +10,8 @@ autoreconf -i && \
 make -j4 && \
 sudo make install && \
 sudo apt build-dep lsp-plugins pulseeffects && sudo apt install ladspa-sdk lv2-dev mda-lv2 rubberband-ladspa swh-lv2 zam-plugins calf-plugins  \
-cd ~/ffmpeg_sources &&  git clone https://github.com/wwmm/pulseeffects.git && cd ~/ffmpeg_sources/pulseeffects && git checkout v4.7.0 && meson _build --prefix=/usr/local && sudo ninja -C _build install
+cd ~/ffmpeg_sources &&  git clone https://github.com/wwmm/pulseeffects.git && cd ~/ffmpeg_sources/pulseeffects && git checkout v4.8.5 && meson _build --prefix=/usr/local && sudo ninja -C _build install &&\
 cd ~/ffmpeg_sources/pulseeffects &&\
-git checkout v4.8.5 && \
 meson _build --prefix=/usr/local &&\ 
-sudo ninja -C _build install
+ninja -C _build install
 
