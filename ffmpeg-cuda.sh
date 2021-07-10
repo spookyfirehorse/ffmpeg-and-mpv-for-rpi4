efab1b4c5a640fa7bff2071c6b0d9280cecafc95
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-07-10 11:51:06 +0200

    Update ffmpeg-cuda.sh

--- ffmpeg-cuda.sh
+++ ffmpeg-cuda.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/FFmpeg/FFmpeg.git  && cd FFmpeg && \
-./configure --prefix=/usr/ --enable-gpl --enable-nonfree  --toolchain=hardened --enable-libvpx  \
+git clone https://github.com/FFmpeg/FFmpeg.git && cd FFmpeg && \
+./configure --prefix=/usr/ --enable-gpl --enable-nonfree --extra-version=4 --toolchain=hardened --enable-libvpx  \
 --enable-cuda --enable-cuda-llvm --enable-vaapi --enable-nvdec --enable-cuvid --enable-nvenc --enable-ffnvcodec --enable-libnpp --disable-stripping \
 --enable-libsoxr --enable-libpulse --enable-gnutls --enable-ladspa --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio \
 --enable-libcodec2 --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack \
@@ -7,8 +7,7 @@ git clone https://github.com/FFmpeg/FFmpeg.git  && cd FFmpeg && \
 --enable-libsnappy --enable-libspeex --enable-libopus  --enable-libtheora --enable-libtwolame --enable-libvorbis  --enable-libwebp \
 --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzmq --disable-doc --disable-htmlpages --disable-manpages --enable-libzvbi --enable-lv2 \
 --enable-omx --enable-openal --enable-opengl --enable-sdl2 --enable-omx --enable-libdc1394 --enable-libdrm --enable-frei0r --enable-libx264  --enable-avisynth \
---enable-libsrt --enable-shared --enable-libfdk-aac  --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-version3 --libdir=/usr/lib/aarch64-linux-gnu --incdir=/usr/include/aarch64-linux-gnu --arch=arm64 \
+--enable-libsrt --enable-shared --enable-libfdk-aac  --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-version3 --libdir=/usr/lib/x86_64-linux-gnu --incdir=/usr/include/x86_64-linux-gnu \
 --enable-liblensfun --enable-libdav1d --enable-lv2 --enable-libwebp --enable-libvpx --enable-libv4l2 --enable-libspeex   \
---enable-librubberband --enable-libaom --enable-libmfx --enable-libsvtav1 --enable-libdav1d  --enable-libkvazaar --enable-pthreads \
+--enable-vapoursynth  --enable-librubberband --enable-libaom --enable-libmfx --enable-libsvtav1 --enable-libdav1d  --enable-libkvazaar --enable-pthreads \
 --enable-libsnappy && make -j4 && sudo make install && make tools/qt-faststart && sudo cp  tools/qt-faststart /usr/bin/ && sudo ldconfig
-
