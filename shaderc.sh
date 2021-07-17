52ce224be75dd513333b254dcaa56043b79ce31f
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-07-17 15:06:40 +0200

    Create shaderc.sh

---
+++ shaderc.sh
@@ -0,0 +1,3 @@
+cd ~/ffmpeg_sources/ && git clone https://github.com/google/shaderc  && \
+mkdir -p shaderc_build && cd shaderc && ./utils/git-sync-deps && cd ~/ffmpeg_sources/shaderc_build && \
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../shaderc && ninja install
