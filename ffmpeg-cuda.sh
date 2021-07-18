e006ed7cf4456ed0e8cda8dcdba909e8a0723004
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-07-18 12:45:49 +0200

    Update ffmpeg-cuda.sh

--- ffmpeg-cuda.sh
+++ ffmpeg-cuda.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/FFmpeg/FFmpeg.git && cd FFmpeg && \
-./configure --prefix=/usr/local --enable-gpl --enable-nonfree --extra-version=4 --toolchain=hardened --enable-libvpx  \
+./configure --prefix=/usr/local --enable-gpl --enable-nonfree  --toolchain=hardened --enable-libvpx  \
 --enable-cuda --enable-cuda-llvm --enable-vaapi --enable-nvdec --enable-cuvid --enable-nvenc --enable-ffnvcodec  --disable-stripping \
 --enable-libsoxr --enable-libpulse --enable-gnutls --enable-ladspa --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio \
 --enable-libcodec2 --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack \
@@ -12,4 +12,4 @@ git clone https://github.com/FFmpeg/FFmpeg.git && cd FFmpeg && \
 --enable-liblensfun --enable-libdav1d --enable-lv2 --enable-libwebp --enable-libvpx --enable-libv4l2 --enable-libspeex   \
 --enable-vapoursynth  --enable-librubberband --enable-libaom --enable-libmfx --enable-libsvtav1 --enable-libdav1d  --enable-libkvazaar --enable-pthreads \
 --enable-libsnappy --enable-shared --enable-libnpp  --enable-v4l2-m2m --extra-cflags=-I/usr/local/cuda/include/ --extra-ldflags=-L/usr/local/cuda/lib64/  \
-  && make -j4 && sudo make install && make tools/qt-faststart && sudo cp  tools/qt-faststart /usr/bin/ && sudo ldconfig
+  && make -j4 && sudo make install && make tools/qt-faststart && sudo cp  tools/qt-faststart /usr/local/bin/ && sudo ldconfig
