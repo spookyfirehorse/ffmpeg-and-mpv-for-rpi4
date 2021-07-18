c5bae64b45bb1f8bf0c1ae40bf554e8e632acb27
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-07-18 12:44:22 +0200

    Update mpv-rpi-3b+-4.sh

--- mpv-rpi-3b+-4.sh
+++ mpv-rpi-3b+-4.sh
@@ -3,6 +3,6 @@ cd ~/ffmpeg_sources && git clone https://github.com/mpv-player/mpv.git && \
 cd ~/ffmpeg_sources/mpv && \
 ./bootstrap.py && \
 export PKG_CONFIG_PATH=/opt/vc/lib/pkgconfig && ./waf configure --enable-rpi --enable-rpi-mmal  --enable-egl-drm --enable-gl-x11 \
---enable-egl --enable-sdl2 --enable-ffmpeg-strict-abi  --enable-xv --enable-zimg --enable-egl-x11 --enable-zimg --enable-libplacebo --enable-vulkan && 
+--enable-egl --enable-sdl2 --enable-ffmpeg-strict-abi  --enable-xv --enable-zimg --enable-egl-x11  --enable-libplacebo --enable-vulkan && 
 ./waf -j4 && sudo  ./waf install 
 
