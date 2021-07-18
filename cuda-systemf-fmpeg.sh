cbaf3e0e4292288cb7548cd7591e2cfc12051d15
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2021-07-18 13:28:28 +0200

    Update cuda-systemf-fmpeg.sh

--- cuda-systemf-fmpeg.sh
+++ cuda-systemf-fmpeg.sh
@@ -8,7 +8,7 @@ apt source ffmpeg && cd ffmpeg && \
 --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzmq --disable-doc --disable-htmlpages --disable-manpages --enable-libzvbi --enable-lv2 \
 --enable-omx --enable-openal --enable-opengl --enable-sdl2 --enable-omx --enable-libdc1394 --enable-libdrm --enable-frei0r --enable-libx264  --enable-avisynth \
 --enable-libsrt --enable-libfdk-aac  --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-version3 \
- --libdir=/usr/lib/x86_64-linux-gnu --incdir=/usr/include/x86_64-linux-gnu \
+ --libdir=/usr/lib/x86_64-linux-gnu --incdir=/usr/include/x86_64-linux-gnu --enable-chromaprint \
 --enable-liblensfun --enable-libdav1d --enable-lv2 --enable-libwebp --enable-libvpx --enable-libv4l2 --enable-libspeex   \
 --enable-vapoursynth  --enable-librubberband --enable-libaom --enable-libmfx  --enable-libdav1d  --enable-libkvazaar --enable-pthreads \
 --enable-libsnappy --enable-shared  --enable-v4l2-m2m --extra-cflags=-I/usr/local/cuda/include/ --extra-ldflags=-L/usr/local/cuda/lib64/ && make -j4 && sudo make install && make tools/qt-faststart && sudo cp  tools/qt-faststart /usr/bin/ && sudo ldconfig
