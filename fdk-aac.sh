fdd15b56c98974947ab62c22beb95576bb885726
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-01-21 19:20:43 +0100

    Update fdk-aac.sh

--- fdk-aac.sh
+++ fdk-aac.sh
@@ -3,6 +3,6 @@ cd ~/ffmpeg_sources && \
 git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
 cd fdk-aac && \
 autoreconf -fiv && \
-./configure  --disable-shared && \
+./configure  --enable-shared && \
 make -j4 && \
 sudo make install && sudo ldconfig
