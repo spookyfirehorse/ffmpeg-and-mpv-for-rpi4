d206c6e8b83a262a64f79c6a1441529ceaf0e747
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-02-15 09:48:09 +0100

    Update mplayer.sh

--- mplayer.sh
+++ mplayer.sh
@@ -1,2 +1,4 @@
-sudo apt build-dep mplayer  && cd ~/ffmpeg_sources/ && svn checkout svn://svn.mplayerhq.hu/mplayer/trunk mplayer && cd mplayer && \
-./configure  --enable-faad --enable-gui    && make -j4 && sudo make install
+sudo apt build-dep mplayer  && \
+svn checkout svn://svn.mplayerhq.hu/mplayer/trunk mplayer && cd mplayer && ./configure  --enable-faad --enable-gui --disable-ffmpeg_a    && \
+make -j4 && sudo make install
+
