3f32830d8199807a546ba4a34a301ff04b6f1f39
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-02-17 12:06:12 +0100

    Update lsp-plugins-4-armhf.sh

--- lsp-plugins-4-armhf.sh
+++ lsp-plugins-4-armhf.sh
@@ -1,5 +1,3 @@
 sudo apt install php-all-dev sndfile-tools libsndfile1-dev && cd ~/ffmpeg_sources/  && \ 
-git clone  https://salsa.debian.org/multimedia-team/lsp-plugins.git && \
-cd ~/ffmpeg_sources/lsp-plugins && ./build-armv7a.sh && \
-make -j4 && \
-sudo make install
+cd ~/ffmpeg_sources && wget http://deb.debian.org/debian/pool/main/l/lsp-plugins/lsp-plugins_1.1.31.orig.tar.gz &&\
+tar -xf lsp-plugins_1.1.31.orig.tar.gz && cd lsp-plugins-src-1.1.31 && ./build-armv7a.sh && make -j4 && sudo make -j4 install
