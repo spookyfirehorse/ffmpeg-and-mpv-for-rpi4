a9e47f9c9bbbf0735302bfe5798b5694d6bbd4ef
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-02-19 10:55:51 +0100

    Update libdrm.sh

--- libdrm.sh
+++ libdrm.sh
@@ -1,3 +1,5 @@
 apt source libdrm
 cd libdrm
-CFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" CXXFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" meson -Dudev=true -Dvc4=true -Dintel=false -Dvmwgfx=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dfreedreno=false -Dinstall-test-programs=true ..
+CFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" CXXFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" meson --prefix /usr -Dudev=true -Dvc4=true -Dintel=false -Dvmwgfx=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dfreedreno=false -Dinstall-test-programs=true build
+ninja -C build
+sudo ninja -C build install
