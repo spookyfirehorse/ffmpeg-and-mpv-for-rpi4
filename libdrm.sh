6c8eb3bd91f0b9fec1ddaf82377f4a64bb306dc4
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-02-19 11:33:47 +0100

    Update libdrm.sh

--- libdrm.sh
+++ libdrm.sh
@@ -1,4 +1,4 @@
-apt source libdrm
+apt source libdrm && sudo apt install libatomic-ops-dev
 cd libdrm
 CFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" CXXFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" meson --prefix /usr -Dudev=true -Dvc4=true -Dintel=false -Dvmwgfx=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dfreedreno=false -Dinstall-test-programs=true build
 ninja -C build
