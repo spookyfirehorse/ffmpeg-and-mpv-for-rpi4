a9e12b376e86c32d37bb84df24e6916b5d96746f
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-02-19 09:58:57 +0100

    Create libdrm.sh

---
+++ libdrm.sh
@@ -0,0 +1,3 @@
+apt source libdrm
+cd libdrm
+CFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" CXXFLAGS="-O2 -march=armv8-a+crc+simd -mtune=cortex-a72" meson -Dudev=true -Dvc4=true -Dintel=false -Dvmwgfx=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dfreedreno=false -Dinstall-test-programs=true ..
