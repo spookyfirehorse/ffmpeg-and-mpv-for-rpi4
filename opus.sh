999ac75b3dc9568a0d521f13d9e45b89852da471
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-02-23 20:09:37 +0100

    Update opus.sh

--- opus.sh
+++ opus.sh
@@ -2,6 +2,6 @@ cd ~/ffmpeg_sources && \
 git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git && \
 cd opus && \
 ./autogen.sh && \
-./configure --prefix=/usr/local --disable-shared && \
+./configure --prefix=/usr/local --enable-shared && \
 make -j4 && \
 sudo make -j4 install
