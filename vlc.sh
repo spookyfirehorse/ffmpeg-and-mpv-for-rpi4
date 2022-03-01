e61e6e2cef1ec75d192b7a34ab231327ebd3f13a
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-03-01 22:06:27 +0100

    Update vlc.sh

--- vlc.sh
+++ vlc.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/jc-kynesim/vlc.git && ./bootstrap && ./configure  --disable-vdpau && make -j4 && sudo make -j4 install
+git clone https://github.com/jc-kynesim/vlc.git && ./bootstrap  && autoreconf -fiv  && ./configure  --enable-mmal  --disable-vdpau --disable-fdkaac --enable-gles2 && make -j4 && sudo make -j4 install
