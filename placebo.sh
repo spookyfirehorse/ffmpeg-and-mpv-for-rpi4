f568e388832ea052b21940b48b17d5b88320bb31
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-03-03 11:24:46 +0100

    Update placebo.sh

--- placebo.sh
+++ placebo.sh
@@ -1,6 +1,9 @@
-cd ~/ffmpeg_sources/ && git clone https://github.com/haasn/libplacebo.git && \
-cd ~/ffmpeg_sources/libplacebo && git switch -c v3.104.0 \ 
-DIR=./build && DIR=./build && meson $DIR  && \
-meson configure $DIR   -Dvulkan=enabled -Dshaderc=enabled  && \
-ninja -C$DIR  && \
-sudo ninja -Cbuild install
+#cd ~/ffmpeg_sources/ && git clone https://github.com/haasn/libplacebo.git && \
+#cd ~/ffmpeg_sources/libplacebo && git switch -c v3.104.0 \ 
+#DIR=./build && DIR=./build && meson $DIR  && \
+#meson configure $DIR   -Dvulkan=enabled -Dshaderc=enabled  && \
+#ninja -C$DIR  && \
+#sudo ninja -Cbuild install
+
+
+sudo apt build-dep libplacebo && apt source libplacebo && cd libplacebo-2.72.2/ && meson build && meson configure build  -Dvulkan=enabled -Dshaderc=enabled &&   ninja -C build && sudo ninja -C build install
