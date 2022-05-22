cc7439db1ef8f283afb85ba1f538dd1271e8e3ec
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2022-05-22 15:50:31 +0200

    Update rubberband.sh

--- rubberband.sh
+++ rubberband.sh
@@ -1,2 +1,2 @@
 cd ~/ffmpeg_sources/ && git clone https://github.com/breakfastquay/rubberband.git && \
-cd ~/ffmpeg_sources/rubberband &&  LDFLAGS=-latomic meson build && ninja -C build && sudo ninja install
+cd ~/ffmpeg_sources/rubberband &&  LDFLAGS=-latomic meson build && ninja -C build && sudo ninja -j4 -C build install
