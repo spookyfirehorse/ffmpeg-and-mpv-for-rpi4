eddc6c4dd793b7f245503e026043554a6aa21cc0
Tomas Rohrer <linux@rohlik.xyz>
2023-03-16 20:06:45 +0100

    Update placebo.sh
    Use proper command

--- placebo.sh
+++ placebo.sh
@@ -1,5 +1,5 @@
 #cd ~/ffmpeg_sources/ && git clone https://github.com/haasn/libplacebo.git && \
-#cd ~/ffmpeg_sources/libplacebo && git switch -c v3.104.0 \ 
+#cd ~/ffmpeg_sources/libplacebo && git checkout tags/v3.104.0 -b v3.104.0 \ 
 #DIR=./build && DIR=./build && meson $DIR  && \
 #meson configure $DIR   -Dvulkan=enabled -Dshaderc=enabled  && \
 #ninja -C$DIR  && \
