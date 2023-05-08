713e10f0e3f3067548ed8269debca25002417aed
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2023-05-08 19:04:33 +0200

    Delete vlc.sh

--- vlc.sh
+++
@@ -1,1 +0,0 @@
-git clone -b dev/3.0.16/drm_1 https://github.com/jc-kynesim/vlc.git && cd vlc && ./bootstrap  && ./configure  --enable-mmal  --disable-vdpau --disable-fdkaac --enable-gles2 && make -j4 && sudo make -j4 install
