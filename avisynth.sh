08042e92ac2568478fa05b73e7bfb83e301dc304
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2023-05-08 19:04:45 +0200

    Delete avisynth.sh

--- avisynth.sh
+++
@@ -1,2 +0,0 @@
-cd ~/ffmpeg_sources && git clone https://github.com/AviSynth/AviSynthPlus.git &&  cd ~/ffmpeg_sources/AviSynthPlus \
-&& mkdir cd ~/ffmpeg_sources/AviSynthPlus/avisynth-build && cmake ../ -DHEADERS_ONLY:bool=on  && sudo make install 
