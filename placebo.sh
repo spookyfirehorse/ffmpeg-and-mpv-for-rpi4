06c942a659779df50a180355bfb67ee5f343f074
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2023-11-04 08:15:19 +0100

    Update placebo.sh

--- placebo.sh
+++ placebo.sh
@@ -6,4 +6,12 @@
 #sudo ninja -Cbuild install
 
 
-sudo apt build-dep libplacebo && apt source libplacebo && cd libplacebo-2.72.2/ && meson build && meson configure build  -Dvulkan=enabled -Dshaderc=enabled &&   ninja -C build && sudo ninja -C build install
+git clone https://github.com/haasn/libplacebo.git
+cd libplacebo
+meson setup build
+meson configure build   -Dvulkan=enabled -Dshaderc=enabled
+meson compile -C build
+sudo meson install -C build
+
+
+sudo apt build-dep libplacebo && apt source libplacebo && cd libplacebo-2.72.2/ && meson build && git clone https://github.com/haasn/libplacebo.git &&   ninja -C build && sudo ninja -C build install
