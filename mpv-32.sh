ffe79950159b72619022b7f6f7ca91d4bf54adc4
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2023-11-05 18:56:07 +0100

    Delete mpv-32.sh

--- mpv-32.sh
+++
@@ -1,1 +0,0 @@
-apt source mpv && cd  mpv-0.32.0  && chmod 777 waf  &&  ./waf configure --enable-egl-drm --enable-gl-x11 --enable-egl --enable-sdl2 --enable-libmpv-shared --disable-libplacebo --disable-vulkan --disable-vaapi --disable-vdpau --enable-ffmpeg-strict-abi  && ./waf -j4 && sudo  ./waf -j4 install
