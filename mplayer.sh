31200d6aec230f7f9fd71920fd778a5784222da4
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2023-12-19 13:44:33 +0100

    Update mplayer.sh

--- mplayer.sh
+++ mplayer.sh
@@ -1,3 +1,3 @@
 #sudo apt build-dep mplayer  && svn checkout svn://svn.mplayerhq.hu/mplayer/trunk mplayer && cd mplayer && ./configure  --enable-faad --enable-gui --disable-ffmpeg_a    && make -j4 && sudo make install
-apt source mplayer && cd mplayer-1.4+ds1/ && ./configure  --enable-faad --enable-gui --enable-dvdnav --disable-ffmpeg_a    && make -j4 && sudo make install
+apt source mplayer && cd mplayer-1.4+ds1/ && ./configure  --enable-faad --enable-gui --enable-dvdnav --disable-ffmpeg_a  --disable-vdpau  && make -j4 && sudo make install
 
