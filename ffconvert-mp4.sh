2e1df2ce158ea395b5b2e6a902c8bd67d07ab6e1
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2023-12-27 00:59:44 +0100

    Update ffconvert-mp4.sh

--- ffconvert-mp4.sh
+++ ffconvert-mp4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-for file in "$1"; do ffmpeg -hwaccel drm  -hwaccel_output_format drm_prime  -i "$file"  -c:v h264_v4l2m2m  -b:v 3M  -num_capture_buffers 92   -num_output_buffers 64 -bufsize 5M   -maxrate 5M  -filter:720:-1 -aspect 16:9 -c:a libfdk_aac  -b:a 96k -f mp4 "${file%.*}_1.mp4"; done
+for file in "$1"; do ffmpeg -hwaccel drm  -hwaccel_output_format drm_prime  -i "$file"  -c:v h264_v4l2m2m  -b:v 3M  -num_capture_buffers 512   -num_output_buffers 64 -bufsize 5M   -maxrate 5M  -filter:720:-1 -aspect 16:9 -c:a libfdk_aac  -b:a 96k -f mp4 "${file%.*}_1.mp4"; done
