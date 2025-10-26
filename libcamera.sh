6ec5b87438ed78545cfc02f0af7fad21aab4af41
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2025-10-26 16:21:56 +0100

    Update libcamera.sh

--- libcamera.sh
+++ libcamera.sh
@@ -23,3 +23,12 @@ systemctl --user stop pipewire.service \
                       pipewire-pulse.socket
 
 
+
+original
+
+git clone https://github.com/raspberrypi/libcamera.git
+cd libcamera
+meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=true -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
+ninja -C build install
+
+-b sync-fix
