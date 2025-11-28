c4e7976b2d1e083b5315ef75bc9624f5c8702a8d
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2025-11-28 12:20:05 +0100

    Update libcamera.sh

--- libcamera.sh
+++ libcamera.sh
@@ -1,3 +1,5 @@
+sudo apt build-dep libcamera rpicam-apps
+
 git clone https://github.com/raspberrypi/libcamera.git
 cd libcamera
 meson setup build --buildtype=release  -Dprefix=/usr  -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=enabled -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
