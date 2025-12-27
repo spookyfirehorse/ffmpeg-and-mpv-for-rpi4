73dcc9f9de96d38c0063572261e79fb2e9a35093
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2025-12-27 21:48:37 +0100

    Update libcamera.sh

--- libcamera.sh
+++ libcamera.sh
@@ -33,30 +33,26 @@ meson setup build --buildtype=release -Dprefix=/usr -Dpipelines=rpi/vc4,rpi/pisp
 ninja -C build install
 
 #######################
-6 houre runntime half second sync dif
+###  rpi 4 10 h test sync
 
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5   --denoise cdn>
-  --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 \
-  --profile=high --hdr=off --libav-video-codec h264_v4l2m2m   --sharpness   1.0  --level 4.2 --framerate 24  --width 1280 --height 720 \
-  --audio-device=alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo --av-sync=0 \
-  --audio-codec aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=96kbps  \
-  -t 0  -n --inline -o  - | ffmpeg -r 24  -hide_banner -fflags nobuffer+genpts  -flags low_delay -hwaccel drm -hwaccel_output_format drm_prime -i -\
-  -metadata title='Lucy'  -vcodec h264_v4l2m2m  -b:v 1M -num_output_buffers 32 -num_capture_buffers 16  -acodec aac -profile:a aac_low \
-  -b:a 32k -threads $(nproc) -fps_mode:v cfr   -filter:v  fps=fps=film:round=near:start_time=30  \
-   -f rtsp -rtsp_transport udp  rtsp://localhost:8554/mystream
+ nice -n -11  rpicam-vid  --low-latency 1  -b 1000000 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5 \
+ --denoise cdn_off   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0    --profile=high --hdr=off --libav-video-codec h264_v4l2m2m \
+ --level 4.2 --framerate 24  --width 1536 --height 864   --audio-device=alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo --av-sync=0  \
+ --audio-codec libfdk_aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=96kbps  \
+ -t 0  -n --inline -o  - | ffmpeg   -hide_banner -fflags nobuffer+genpts  -flags low_delay -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='Lucy'  -vcodec h264_v4l2m2m \
+ -b:v 1M -num_output_buffers 32 -num_capture_buffers 16  -acodec libfdk_aac -profile:a aac_he_v2   -b:a 64k -threads $(nproc) -fps_mode:v cfr  -copytb 1   \
+ -f rtsp -rtsp_transport udp
 
-   test 2 runns on all rpi over 3 houre without av dif
 
-   #!/bin/bash
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5   --denoise cdn_off \
-  --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0  \
-  --profile=high --hdr=off --libav-video-codec h264_v4l2m2m   --level 4.2 --framerate 24  --width 1536 --height 864 \
-  --audio-device=alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo --av-sync=0 \
-  --audio-codec libfdk_aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=96kbps  \
-  -t 0  -n --inline -o  - | ffmpeg   -hide_banner -fflags nobuffer+genpts  -flags low_delay -hwaccel drm -hwaccel_output_format drm_prime -i -\
-  -metadata title='Lucy'  -vcodec h264_v4l2m2m  -b:v 1M -num_output_buffers 32 -num_capture_buffers 16  -acodec libfdk_aac -profile:a aac_he_v2 \
-  -b:a 64k -threads $(nproc) -fps_mode:v cfr   -filter:v  fps=fps=film:round=near:start_time=0  \
-   -f rtsp -rtsp_transport udp  rtsp://localhost:8554/mystream
+## rpi 3 10 h test sync
+
+nice -n -11  rpicam-vid  --low-latency 1  -b 1000000    --denoise cdn_off   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0  \
+--profile=high --hdr=off --libav-video-codec h264_v4l2m2m  --level 4.2 --framerate 24  --width 1536 --height 864 \
+--audio-device==alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback  --av-sync=0 \
+--audio-codec libfdk_aac  --audio-channels 1 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=128kbps --libav-video-codec-opts bf=0 --intra 0    \
+  -t 0  -n --inline -o  - | ffmpeg -r 23.976  -hide_banner -fflags nobuffer+genpts  -flags low_delay  \
+  -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='kali'  -vcodec copy -copytb 1  -acodec libfdk_aac -eld_v2 1  -vbr 0  -b:a 64k   \
+   -f rtsp -rtsp_transport udp  
 
 
 ######################################################
@@ -94,8 +90,8 @@ rtsp-transport=udp
 oset-metadata=title="Lucy",comment="stream"
 audio-format=s16
 audio-samplerate=48000
-vo-null-fps=23.976
-#vo-null-fps=25
+vo-null-fps=24  # rpi4
+#vo-null-fps=23.976 # rpi 3
 #demuxer-lavf-hacks=yes
 hr-seek-framedrop=no
 video-sync=audio
@@ -106,65 +102,3 @@ demuxer=lavf
 #speed=1.001
 
 
-5h test rpi4
-
-
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5   --denoise cdn_off \
-  --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0  \
-  --profile=high --hdr=off --libav-video-codec h264_v4l2m2m   --level 4.2 --framerate 24  --width 1536 --height 864 \
-  --audio-device=alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo --av-sync=0 \
-  --audio-codec libfdk_aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=96kbps  \
-  -t 0  -n --inline -o  - | ffmpeg   -hide_banner -fflags nobuffer+genpts  -flags low_delay -hwaccel drm -hwaccel_output_format drm_prime -i -\
-  -metadata title='Lucy'  -vcodec h264_v4l2m2m  -b:v 1M -num_output_buffers 32 -num_capture_buffers 16  -acodec libfdk_aac -profile:a aac_he_v2 \
-  -b:a 64k -threads $(nproc) -fps_mode:v cfr   -filter:v  fps=fps=film:round=near:start_time=0  \
-   -f rtsp -rtsp_transport udp
-   
-next +copytb rpi 4
-
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000 --autofocus-mode manual --autofocus-range normal --autofocus-window  0.25,0.25,0.5,0.5   --denoise cdn_off \  
-   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0    --profile=high --hdr=off --libav-video-codec h264_v4l2m2m   --level 4.2 --framerate 24  --width 1536 --height 864 \
-  --audio-device=alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo --av-sync=0 \ 
-  --audio-codec libfdk_aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=96kbps  \
-  -t 0  -n --inline -o  - | ffmpeg   -hide_banner -fflags nobuffer+genpts  -flags low_delay \
-  -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='Lucy'  -vcodec h264_v4l2m2m  -b:v 1M -num_output_buffers 32 -num_capture_buffers 16 \
-  -acodec libfdk_aac -profile:a aac_he_v2   -b:a 64k -threads $(nproc) -fps_mode:v cfr  -copytb 1  \
-   -f rtsp -rtsp_transport udp
-   
-12h test rpi 3 z2w
-
-
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000    --denoise cdn_off   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0    --profile=high --hdr=off --libav-video-codec h264_v4l2m2m \
-  --level 4.2 --framerate 24  --width 1536 --height 864   --audio-device==alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback  --av-sync=0 \
-  --audio-codec libfdk_aac  --audio-channels 1 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=128kbps --libav-video-codec-opts bf=0 --intra 0    \
-   -t 0  -n --inline -o  - | ffmpeg -r 23.976  -hide_banner -fflags nobuffer+genpts  -flags low_delay  \
-  -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='kali'  -vcodec copy -copytb 1  -acodec libfdk_aac -eld_v2 1  -vbr 0  -b:a 64k   \
-   -f rtsp -rtsp_transport udp 
-
-
-not tested rpi 3
-
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000    --denoise cdn_off   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0    --profile=high --hdr=off --libav-video-codec h264_v4l2m2m \
-  --level 4.2 --framerate 24  --width 1536 --height 864   --audio-device==alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback  --av-sync=0 \
-  --audio-codec libfdk_aac  --audio-channels 1 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=128kbps --libav-video-codec-opts bf=0 --intra 0    \
-   -t 0  -n --inline -o  - | ffmpeg -r 23.976  -hide_banner -fflags nobuffer+genpts  -flags low_delay  \
-  -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='kali'  -codec copy -copytb 1    \
-   -f rtsp -rtsp_transport udp  
-
-   
-not tested rpi 4
-
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000    --denoise cdn_off   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0    --profile=high --hdr=off --libav-video-codec h264_v4l2m2m \
-  --level 4.2 --framerate 24  --width 1536 --height 864   --audio-device==alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo  --av-sync=0 \
-  --audio-codec libfdk_aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=128kbps --libav-video-codec-opts bf=0 --intra 0    \
-   -t 0  -n --inline -o  - | ffmpeg -r 24  -hide_banner -fflags nobuffer+genpts  -flags low_delay  \
-  -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='lucy'  -codec copy -copytb 1    \
-   -f rtsp -rtsp_transport udp
-
-   not tested rpi 4
-
-nice -n -11  rpicam-vid  --low-latency 1  -b 1000000    --denoise cdn_off   --codec libav --libav-format flv  --brightness 0.1 --contrast 1.0 --sharpness   1.0    --profile=high --hdr=off --libav-video-codec h264_v4l2m2m \
-  --level 4.2 --framerate 24  --width 1536 --height 864   --audio-device==alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00229929-00.analog-stereo  --av-sync=0 \
-  --audio-codec libfdk_aac  --audio-channels 2 --libav-audio 1 --audio-source pulse --audio-samplerate=48000  --audio-bitrate=128kbps --libav-video-codec-opts bf=0 --intra 0    \
-   -t 0  -n --inline -o  - | ffmpeg  -hide_banner -fflags nobuffer+genpts  -flags low_delay  \
-  -hwaccel drm -hwaccel_output_format drm_prime -i -  -metadata title='lucy'  -codec copy -copytb 1    \
-   -f rtsp -rtsp_transport udp
