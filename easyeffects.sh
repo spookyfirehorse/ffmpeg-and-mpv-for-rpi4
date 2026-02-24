e48670e6174b43dd83769262176dacfc9c1b8d59
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-02-24 22:02:54 +0100

    Update easyeffects.sh

--- easyeffects.sh
+++ easyeffects.sh
@@ -3,3 +3,7 @@ meson _build --prefix=/usr
 sudo ninja -C _build install
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/JackHack96/PulseEffects-Presets/master/install.sh)"
+
+
+CXXFLAGS="-march=goldmont -O3 -pipe" meson setup build --prefix /usr --buildtype release -Db_lto=true
+
