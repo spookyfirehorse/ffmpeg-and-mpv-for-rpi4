a5de6c419f188fc751354397b777f35c03376697
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-03-05 22:11:15 +0100

    Update ffmpeg-intel.sh

--- ffmpeg-intel.sh
+++ ffmpeg-intel.sh
@@ -1,7 +1,3 @@
-sudo apt install libgnutls28-dev libp11-kit-dev libzstd-dev libidn2-dev libunistring-dev
-
-
-
 1. Nach Hardware-Filtern suchen (CUDA/NPP)
 Die Nvidia-Filter liegen in libavfilter/. So findest du die Definitionen für das Hardware-Scaling oder Deinterlacing:
 bash
@@ -88,21 +84,21 @@ Das "Universal-Nvidia-Skylake" Skript:
 Kopier dir das hier in eine Datei (z.B. build_ffmpeg.sh), mach sie mit chmod +x ausführbar und du hast ein Skript für beide Welten:
 bash
 
-nvidia
 
-#!/bin/bash
+ubuntu shaderc test c true
+
+
 make distclean && \
-PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH" \
+PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig" \
 ./configure --prefix=/usr \
  --libdir=/usr/lib/x86_64-linux-gnu \
  --incdir=/usr/include/x86_64-linux-gnu \
- --extra-version="ultra-skylake-universal-C-support" \
- --arch=x86_64 \
- --cpu=skylake \
+ --extra-version="ultra-skylake-8.0.1-FINAL-C" \
+ --arch=x86_64 --cpu=skylake \
+ --pkg-config-flags="--static" \
  --extra-cflags='-march=skylake -O3 -pipe -fPIC -I/usr/include/vulkan' \
- --extra-cxxflags='-march=skylake -O3 -pipe -fPIC' \
- --extra-ldflags='-L/usr/lib/x86_64-linux-gnu -Wl,-O1 -Wl,-rpath,/usr/lib/x86_64-linux-gnu -Wl,--as-needed' \
- --extra-libs='-lpthread -lm -lrt -ldl -lstdc++ -lxml2 -lz' \
+ --extra-ldflags='-L/usr/lib/x86_64-linux-gnu -Wl,-rpath,/usr/lib/x86_64-linux-gnu -Wl,--as-needed' \
+ --extra-libs='-lpthread -lm -lrt -ldl -lstdc++ -lxml2 -lz -lgnutls -lnettle -lhogweed -lgmp -lidn2 -lunistring -lp11-kit' \
  --nvcc=/usr/bin/nvcc \
  --disable-everything --disable-hwaccels \
  --enable-ffmpeg --enable-ffprobe --enable-avdevice --enable-avcodec --enable-avformat --enable-avfilter --enable-swresample --enable-swscale \
@@ -123,27 +119,21 @@ PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/x86_64-linux
  --enable-muxer='mp4,mkv,mov,avi,flac,wav,mp3,opus,ogg,rtsp,rtp,rtmp,flv,hls,mpegts,adts,dash,matroska,image2,null,ac3,eac3,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt' \
  --enable-decoder='h264,hevc,vp9,av1,mjpeg,mpeg1video,mpeg2video,mpeg4,h264_cuvid,hevc_cuvid,vp9_cuvid,aac,mp3,flac,vorbis,opus,pcm_s16le,pcm_s24le,pcm_s32le,rawvideo,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt,adpcm_swf,ass,srt' \
  --enable-encoder='h264_nvenc,hevc_nvenc,av1_nvenc,h264_vaapi,hevc_vaapi,libfdk_aac,libmp3lame,libopus,libvorbis,libx264,libx265,ac3,eac3,flac,vorbis,pcm_s16le,pcm_s24le,pcm_s32le,rawvideo,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt,ass,srt' \
- --enable-filter='afifo,anull,asplit,amix,aresample,aformat,volume,loudnorm,equalizer,crystalizer,ladspa,lv2,rubberband,scale,format,fps,crop,overlay,drawtext,hwupload_cuda,hwdownload,scale_npp,yadif_cuda,libplacebo' \
+ --enable-filter='afifo,anull,asplit,amix,aresample,aformat,volume,loudnorm,equalizer,crystalizer,ladspa,lv2,rubberband,scale,format,fps,crop,overlay,drawtext,hwupload,hwdownload,hwmap,nullsrc,hwupload_cuda,hwdownload_cuda,scale_npp,yadif_cuda,libplacebo' \
  --enable-parser='h264,hevc,vp9,mjpeg,mpegvideo,mpeg4video,aac,mp3,flac,opus,vorbis,ac3,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt' \
  --enable-bsf='h264_mp4toannexb,hevc_mp4toannexb,mpeg4_unpack_bframes,aac_adtstoasc,extract_extradata,null,opus_metadata' \
  --enable-asm --enable-inline-asm --enable-runtime-cpudetect --disable-indev=pulse --disable-outdev='pulse,xv' \
  && make -j$(nproc) && sudo make install && sudo ldconfig
-Verwende Code mit Vorsicht.
 
-
-intel 
-
-Das korrigierte Universal-Skript für Intel (Goldmont)
-Ich habe hier die VAAPI-Hardware-Konverter und die Vulkan-Interoperabilität so verzahnt, dass du auch auf dem Goldmont das „C“ bei libplacebo bekommst:
-bash
 make distclean && \
 PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH" \
 ./configure --prefix=/usr \
   --libdir=/usr/lib/x86_64-linux-gnu \
   --incdir=/usr/include/x86_64-linux-gnu \
-  --extra-version="ultra-goldmont-8.0.1-C-support" \
+  --extra-version="ultra-goldmont-8.0.1-FINAL-C" \
   --arch=x86_64 \
   --cpu=goldmont \
+  --pkg-config-flags="--static" \
   --extra-cflags="-march=goldmont -mtune=goldmont -O3 -pipe -fPIC -I/usr/include/vulkan" \
   --extra-cxxflags="-march=goldmont -mtune=goldmont -O3 -pipe -fPIC" \
   --extra-ldflags="-L/usr/lib/x86_64-linux-gnu -Wl,-O1 -Wl,-rpath,/usr/lib/x86_64-linux-gnu -Wl,--as-needed" \
@@ -163,17 +153,12 @@ PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/x86_64-linux
   --enable-indev='alsa,fbdev,jack,kmsgrab,lavfi,pipe,v4l2' \
   --enable-outdev='alsa,drm,fbdev,jack,kmsgrab,v4l2,pipe' \
   --enable-protocol='cache,concat,crypto,dash,fd,ffrtmpcrypt,file,hls,http,https,pipe,rtmp,rtmpt,rtmpe,rtmps,rtmpt,rtp,rtsp,srt,tcp,tls,udp,unix' \
-  --enable-demuxer='aac,avi,concat,dash,flac,h264,hevc,hls,image2,lavfi,matroska,mjpeg,mov,mp3,mpegts,mpegvideo,ogg,rawvideo,rtmp,rtp,rtsp,sdp,wav' \
-  --enable-muxer='adts,avi,dash,flac,hls,image2,matroska,mov,mp3,mp4,mpegts,null,ogg,opus,rawvideo,rtmp,rtp,rtsp,wav' \
-  --enable-decoder='mov_text,dvdsub,dvbsub,h263,h264,hevc,vp9,av1,mjpeg,mpeg1video,mpeg2video,mpeg4,aac,mp3,flac,vorbis,opus,pcm_s16le,pcm_s24le,pcm_s32le,rawvideo,ass,srt,h264_vaapi,hevc_vaapi,vp9_vaapi' \
-  --enable-encoder='mov_text,libfdk_aac,libmp3lame,libopus,libvorbis,libx264,libx265,h264_vaapi,hevc_vaapi,vp9_vaapi,ac3,eac3,flac,pcm_s16le,pcm_s24le,rawvideo,ass,srt' \
-  --enable-filter='hwupload,hwdownload,format,fps,scale,crop,overlay,drawtext,aresample,volume,loudnorm,equalizer,crystalizer,ladspa,lv2,rubberband,scale_vaapi,deinterlace_vaapi,denoise_vaapi,sharpness_vaapi,procamp_vaapi,tonemap_vaapi,yadif_vaapi,libplacebo,hwmap' \
+  --enable-demuxer='mov,matroska,flac,wav,mp3,ogg,aac,avi,h264,hevc,hls,image2,lavfi,mpegts,mpegvideo,rawvideo,rtmp,rtp,rtsp,sdp,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt' \
+  --enable-muxer='adts,avi,dash,flac,hls,image2,matroska,mov,mp3,mp4,mpegts,null,ogg,opus,rawvideo,rtmp,rtp,rtsp,wav,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt' \
+  --enable-decoder='mov_text,dvdsub,dvbsub,h263,h264,hevc,vp9,av1,mjpeg,mpeg1video,mpeg2video,mpeg4,aac,mp3,flac,vorbis,opus,pcm_s16le,pcm_s24le,pcm_s32le,rawvideo,ass,srt,h264_vaapi,hevc_vaapi,vp9_vaapi,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt' \
+  --enable-encoder='mov_text,libfdk_aac,libmp3lame,libopus,libvorbis,libx264,libx265,h264_vaapi,hevc_vaapi,vp9_vaapi,ac3,eac3,flac,pcm_s16le,pcm_s24le,rawvideo,ass,srt,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt' \
+  --enable-filter='afifo,anull,asplit,amix,aresample,aformat,volume,loudnorm,equalizer,crystalizer,ladspa,lv2,rubberband,scale,format,fps,crop,overlay,drawtext,hwupload,hwdownload,hwmap,nullsrc,scale_vaapi,deinterlace_vaapi,denoise_vaapi,sharpness_vaapi,procamp_vaapi,tonemap_vaapi,yadif_vaapi,setdar,settb,setpts,asettb,asetpts,concat,pad,pan,yadif,asubboost,asubcut,libplacebo' \
   --enable-parser='aac,flac,h263,h264,hevc,mjpeg,mp3,mpegaudio,mpegvideo,mpeg4video,opus,vorbis' \
   --enable-bsf='aac_adtstoasc,extract_extradata,h264_mp4toannexb,hevc_mp4toannexb,mpeg4_unpack_bframes,null,opus_metadata' \
   --enable-asm --enable-x86asm --enable-inline-asm --enable-runtime-cpudetect \
   && make -j$(nproc) && sudo make install && sudo ldconfig
-Verwende Code mit Vorsicht.
-
-
-
-
