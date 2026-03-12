7fc5855ea648d28cba31ca0a7b9ac40782d804d0
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-03-12 10:25:54 +0100

    Update build-ffmpeg--final-cuda.sh

--- build-ffmpeg--final-cuda.sh
+++ build-ffmpeg--final-cuda.sh
@@ -41,13 +41,13 @@ PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH" \
 --disable-cuda --enable-nvenc --enable-cuda-llvm --enable-nvdec --enable-cuvid  --enable-libnpp --enable-ffnvcodec --enable-cuda-nvcc --enable-libnpp --enable-ffnvcodec  \
 --enable-vaapi --enable-vulkan --enable-libplacebo --enable-libshaderc --enable-opengl --enable-libdrm --enable-libjack --disable-vdpau \
 --enable-libdvdnav --enable-libdvdread \
---enable-hwaccel='vp9_vulkan,h264_nvdec,hevc_nvdec,vp9_nvdec,av1_nvdec,mjpeg_nvdec,mpeg1_nvdec,mpeg2_nvdec,mpeg4_nvdec,vc1_nvdec,h264_vulkan,hevc_vulkan,av1_vulkan' \
+--enable-hwaccel='vp9_vulkan,h264_nvdec,hevc_nvdec,vp9_nvdec,av1_nvdec,mjpeg_nvdec,mpeg1_nvdec,mpeg2_nvdec,mpeg4_nvdec,vc1_nvdec,h264_vulkan,hevc_vulkan,av1_vulkan,prores_raw_vulkan,ffv1_vulkan' \
 --enable-gnutls --enable-gmp --enable-gcrypt --enable-network \
 --enable-libvpx --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-libvorbis \
 --enable-libfreetype --enable-libharfbuzz --enable-libfribidi --enable-hardcoded-tables \
 --enable-ladspa --enable-lv2 --enable-librubberband --enable-libsoxr --enable-libbs2b --enable-libmysofa --enable-alsa --enable-libjack \
 --enable-libx264 --enable-libx265 --enable-xlib --disable-sdl2 --disable-libpulse \
---enable-indev='v4l2,alsa,jack,lavfi,fbdev,xcbgrab,kmsgrab' \
+--enable-indev='v4l2,alsa,jack,lavfi,fbdev,xcbgrab,kmsgrab,xv' \
 --enable-outdev='sdl2,v4l2,alsa,fbdev' \
 --enable-protocol='async,bluray,cache,concat,crypto,data,ffrtmpcrypt,file,ftp,hls,http,https,httpproxy,icecast,md5,pipe,rtmp,rtmpe,rtmps,rtmpt,rtmpte,rtp,srtp,subfile,tcp,tls,udp,udplite,unix' \
 --enable-demuxer='aac,ac3,adpcm_ima_qt,adpcm_ima_wav,adpcm_ms,aiff,asf,avi,dash,dts,dtshd,eac3,flac,flv,h264,hevc,hls,image2,matroska,mjpeg,mov,mp3,mpegps,mpegts,mpegvideo,null,ogg,opus,pcm_alaw,pcm_f32be,pcm_f32le,pcm_mulaw,pcm_s16be,pcm_s16le,pcm_s24be,pcm_s24le,pcm_s32be,pcm_s32le,rawvideo,rm,rtp,rtsp,srt,truehd,vobsub,wav,webm_dash_manifest,dvdnav,dvdread' \
@@ -57,7 +57,7 @@ PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH" \
 --enable-filter='adelay,aecho,afifo,amix,anull,aresample,aformat,asetpts,asplit,colorspace,crystalizer,crop,drawtext,equalizer,firequalizer,format,format_cuda,fps,hwdownload,hwdownload_cuda,hwmap,hwupload,hwupload_cuda,ladspa,libplacebo,loudnorm,lv2,null,nullsrc,overlay,palettegen,paletteuse,pan,rubberband,scale,scale_cuda,scale_npp,setpts,transpose,volume,yadif_cuda' \
 --enable-parser='h264,hevc,vp9,av1,mjpeg,mpegvideo,mpeg4video,aac,mp3,flac,opus,vorbis,ac3,dca,adpcm_ms,adpcm_ima_wav,adpcm_ima_qt,dvdsub,dvd_pel' \
 --enable-bsf='aac_adtstoasc,av1_metadata,extract_extradata,h264_metadata,h264_mp4toannexb,hevc_metadata,hevc_mp4toannexb,mjpeg2jpeg,mpeg4_unpack_bframes,null,opus_metadata,pcm_rechunk,vp9_metadata,vp9_superframe' \
---enable-asm --enable-x86asm --enable-inline-asm --enable-runtime-cpudetect --disable-indev=pulse --disable-outdev='pulse,xv' \
+--enable-asm --enable-x86asm --enable-inline-asm --enable-runtime-cpudetect --disable-indev=pulse --disable-outdev='pulse' \
 && make -j$(nproc) && sudo make install && sudo ldconfig
 
 
