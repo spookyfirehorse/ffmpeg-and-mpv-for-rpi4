7020d4a3f546c2dd9b8e0e0356fa8025435596c1
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-03-15 21:17:31 +0100

    Update build-ffmpeg-final-intel.sh

--- build-ffmpeg-final-intel.sh
+++ build-ffmpeg-final-intel.sh
@@ -52,8 +52,8 @@ sudo strip --strip-unneeded /usr/lib/x86_64-linux-gnu/libavutil*
 sudo strip --strip-unneeded /usr/lib/x86_64-linux-gnu/libpostpro*
 sudo strip --strip-unneeded /usr/lib/x86_64-linux-gnu/libswresampl*
 sudo strip --strip-unneeded /usr/lib/x86_64-linux-gnu/libswscale*
-ls -l /usr/lib/x86_64-linux-gnu/libav* /usr/lib/x86_64-linux-gnu/libsw* /usr/lib/x86_64-linux-gnu/libpostproc* | awk '{sum += $5} END {print sum / 1024 / 1024 " MB"}'
 
+stat -Lc %s  /usr/lib/x86_64-linux-gnu/lib{avcodec,avdevice,avfilter,avformat,avutil,postproc,swresample,swscale}.so | awk '{sum += $1} END {printf "Gesamtgröße armhf: %.2f MB\n", sum/1024/1024}'
    
     sudo ldconfig
     echo "Build & Stripping erfolgreich abgeschlossen!"
