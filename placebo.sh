37fe58046e692e07cd59143cbd4a98e11ca38575
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-03-16 14:17:57 +0100

    Update placebo.sh

--- placebo.sh
+++ placebo.sh
@@ -17,3 +17,27 @@ meson compile -C build
 sudo meson install -C build
 
 
+git clone https://github.com/haasn/libplacebo.git
+cd libplacebo
+git submodule update --init
+
+# Lösche einen alten Build-Ordner, um sauber neu zu starten
+rm -rf build
+
+# Setup mit CPU-Optimierungen für Cortex-A72
+CFLAGS="-mcpu=cortex-a72+crypto -Ofast -pipe -ftree-vectorize" \
+CXXFLAGS="-mcpu=cortex-a72+crypto -Ofast -pipe -ftree-vectorize" \
+meson setup build \
+  --prefix=/usr \
+  --buildtype=release \
+  -Dvulkan=enabled \
+  -Dshaderc=enabled \
+  -Dglslang=enabled \
+  -Db_lto=true \
+  -Db_pie=true
+
+# Kompilieren
+meson compile -C build
+
+# Installieren
+sudo meson install -C build
