f6399f352f4df7076b1e617adee2307cc0c0b590
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-03-16 19:24:53 +0100

    Update build-ffmpeg-final-rpi-4.sh

--- build-ffmpeg-final-rpi-4.sh
+++ build-ffmpeg-final-rpi-4.sh
@@ -63,8 +63,10 @@ meson setup build \
   -Dc_args='-mcpu=cortex-a72+crypto -O3 -pipe -ftree-vectorize -flto -Wno-stringop-overflow' \
   -Dcpp_args='-mcpu=cortex-a72+crypto -O3 -pipe -ftree-vectorize -flto -Wno-stringop-overflow' \
   -Dc_link_args='-L/usr/lib/aarch64-linux-gnu -latomic -Wl,-O3,--as-needed -flto' \
-  -Dcpp_link_args='-L/usr/lib/aarch64-linux-gnu -latomic -Wl,-O3,--as-needed -flto -lstdc++ -Wl,--start-group -lshaderc_combined -lglslang -lMachineIndependent -lGenericCodeGen -lSPIRV -lSPIRV-Tools-opt -lSPIRV-Tools -Wl,--end-group -lm -lpthread' \
-   -Dwayland=enabled \
+  -Dcpp_link_args='-L/usr/lib/aarch64-linux-gnu -latomic -Wl,-O3,--as-needed -flto -lstdc++ \
+  -lplacebo -lvulkan -lgbm -ldrm -lepoxy -lEGL -lGLESv2 \
+  -Wl,--start-group -lshaderc_combined -lglslang -lMachineIndependent -lGenericCodeGen -lSPIRV -lSPIRV-Tools-opt -lSPIRV-Tools -Wl,--end-group \
+  -lm -lpthread' \   -Dwayland=enabled \
   -Ddmabuf-wayland=enabled \
   -Dvulkan=enabled \
   -Dshaderc=disabled \
