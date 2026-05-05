af50d4fd9b2e9a6ef7b11c410dec55c52ff852c1
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-05-05 13:15:49 +0200

    Delete crx directory

--- crx/content_script.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === "GPS_UPDATE") {
-        window.dispatchEvent(new CustomEvent("GPSD_UPDATE_EVENT", {
-            detail: message.data
-        }));
-    }
-});
