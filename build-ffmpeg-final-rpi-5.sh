cca61d77e7610f1265b63d6bfafe3bc79f82f83f
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-05-21 19:24:53 +0200

    Update build-ffmpeg-final-rpi-5.sh

--- build-ffmpeg-final-rpi-5.sh
+++ build-ffmpeg-final-rpi-5.sh
@@ -20,8 +20,8 @@ PKG_CONFIG_PATH="/usr/lib/aarch64-linux-gnu/pkgconfig" \
   --incdir=/usr/include/aarch64-linux-gnu \
   --extra-version=ultra-pi5 \
   --arch=aarch64 --target-os=linux --cpu=cortex-a76 \
-  --extra-cflags='-I/usr/include/libdrm -I/usr/include/gbm -I/usr/include/vulkan -mcpu=cortex-a76+crypto -Ofast -fomit-frame-pointer -falign-functions=32 -falign-loops=32 -mlow-precision-div -pipe -ftree-vectorize -flto -fno-semantic-interposition -Wno-stringop-overflow' \
-  --extra-ldflags='-L/usr/lib/aarch64-linux-gnu -lgbm -ldrm -Wl,-O3 -Wl,--hash-style=gnu -Wl,--as-needed -Ofast -flto -latomic ' \
+  --extra-cflags='-I/usr/include/libdrm -I/usr/include/gbm -I/usr/include/vulkan -mcpu=cortex-a76+crypto -Ofast -fomit-frame-pointer -falign-functions=32 -falign-loops=32 -mlow-precision-div -pipe -ftree-vectorize  -fno-semantic-interposition -Wno-stringop-overflow' \
+  --extra-ldflags='-L/usr/lib/aarch64-linux-gnu -lgbm -ldrm -Wl,-O3 -Wl,--hash-style=gnu -Wl,--as-needed -Ofast -latomic ' \
   --extra-libs='-Wl,--start-group -lshaderc_combined -lglslang -lMachineIndependent -lGenericCodeGen -lSPIRV -lSPIRV-Tools-opt -lSPIRV-Tools -Wl,--end-group -lplacebo -lvulkan -lgbm -ldrm -lepoxy -lEGL -lGLESv2 -lfftw3 -lgnutls -lxml2 -lfdk-aac -lmp3lame -lFLAC -lopus -lass -lsoxr -lz -lsrt -ldvdnav -lstdc++ -lpthread -lm -lrt -ldl' \
   --disable-everything --disable-hwaccels --enable-libdvdnav --enable-libdvdread \
  --enable-ffmpeg --enable-ffprobe --enable-ffplay --enable-sdl2 --enable-shared --disable-static --disable-debug --enable-stripping --enable-hardcoded-tables --enable-pic --enable-pthreads --enable-gpl --enable-version3 --enable-nonfree --enable-lto --enable-avcodec --enable-avformat --enable-avfilter --enable-swresample --enable-swscale --enable-avdevice \
